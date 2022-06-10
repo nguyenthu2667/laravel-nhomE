158b1a6f7db0f1df96c6b588f8e7ecb48acae565
nguyenthu2667 <hoangpro22@gmail.com>
2022-06-11 03:15:13 +0700

    admin home

--- example-app/resources/views/Admin/adminhome.blade.php
+++ example-app/resources/views/Admin/adminhome.blade.php
@@ -1,12 +1,76 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    home admin
-</body>
-</html>
+@extends('dashboard')
+@section('content')
+    @auth()
+        <div class="wrapper">
+            @include('layouts.navbars.sidebar')
+            <div class="main-panel">
+                @include('layouts.navbars.navbar')
+
+                <div class="content">
+                    @yield('content')
+                </div>
+
+                @include('layouts.footer')
+            </div>
+        </div>
+        <form id="logout-form" action="{{ route('signout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+    @else
+        @include('layouts.navbars.navbar')
+        <div class="wrapper wrapper-full-page">
+            <div class="full-page {{ $contentClass ?? '' }}">
+                <div class="content">
+                    <div class="container">
+                        @yield('content')
+                    </div>
+                </div>
+                @include('layouts.footer')
+            </div>
+        </div>
+    @endauth
+    <div class="fixed-plugin">
+        <div class="dropdown show-dropdown">
+            <a href="#" data-toggle="dropdown">
+                <i class="fa fa-cog fa-2x"> </i>
+            </a>
+            <ul class="dropdown-menu">
+                <li class="header-title"> Sidebar Background</li>
+                <li class="adjustments-line">
+                    <a href="javascript:void(0)" class="switch-trigger background-color">
+                        <div class="badge-colors text-center">
+                            <span class="badge filter badge-primary active" data-color="primary"></span>
+                            <span class="badge filter badge-info" data-color="blue"></span>
+                            <span class="badge filter badge-success" data-color="green"></span>
+                        </div>
+                        <div class="clearfix"></div>
+                    </a>
+                </li>
+                <li class="button-container">
+                    <a href="https://www.creative-tim.com/product/black-dashboard-laravel" target="_blank"
+                        class="btn btn-primary btn-block btn-round">Download Now</a>
+                    <a href="https://demos.creative-tim.com/black-dashboard/docs/1.0/getting-started/introduction.html"
+                        target="_blank" class="btn btn-default btn-block btn-round">
+                        Documentation
+                    </a>
+                    <a href="https://www.creative-tim.com/product/black-dashboard-pro-laravel" target="_blank"
+                        class="btn btn-danger btn-block btn-round">
+                        Upgrade to PRO
+                    </a>
+                </li>
+                <li class="header-title">Thank you for 95 shares!</li>
+                <li class="button-container text-center">
+                    <button id="twitter" class="btn btn-round btn-info"><i class="fab fa-twitter"></i> &middot;
+                        45</button>
+                    <button id="facebook" class="btn btn-round btn-info"><i class="fab fa-facebook-f"></i> &middot;
+                        50</button>
+                    <br>
+                    <br>
+                    <a class="github-button" href="https://github.com/creativetimofficial/black-dashboard-laravel"
+                        data-icon="octicon-star" data-size="large" data-show-count="true"
+                        aria-label="Star ntkme/github-buttons on GitHub">Star</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+@endsection
