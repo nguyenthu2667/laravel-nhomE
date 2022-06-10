bbeab2e7853dc2c841bb95b16d473ce387b56249
ngoquangkhoanqkey <ngoquangkhoanqk@gmail.com>
2022-06-10 15:30:17 +0700

    compelete function contact

---
+++ example-app/app/Http/Controllers/EmailController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+
+class EmailController extends Controller
+{
+    public function create()
+    {
+        return view('email');
+    }
+
+    public function sendEmail(Request $request)
+    {
+      // dd($request);
+        $request->validate([
+        // 'name' => 'required',
+          'email' => 'required|email',
+          'subject' => 'required',
+          'content' => 'required',
+        ]);
+
+        $data = [
+          'subject' => $request->subject,
+        //   'name' => $request->name,
+          'email' => $request->email,
+          'content' => $request->content
+        ];
+
+        Mail::send('email-template', $data, function($message) use ($data) {
+          $message->from("ngoquangkhoanqk@gmail.com",'Khoa dep trai');
+          $message->to($data['email'])
+          ->subject($data['subject']);
+        });
+
+        return back()->with(['message' => 'Gửi Email Thành Công']);
+
+        // Mail::send('email-template', data,fun)
+    }
+}
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -14,4 +14,10 @@ Route::get('registration', [CustomAuthController::class, 'registration'])->name(
 Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
 Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
 Route::get('home', [CustomAuthController::class, 'home'])->name('home');
-Route::get('pagehome', [CustomAuthController::class, 'pagehome'])->name('pagehome');
+Route::get('/', [CustomAuthController::class, 'pagehome'])->name('pagehome');
+
+Route::get('/email', [App\Http\Controllers\EmailController::class, 'create']);
+Route::post('/email', [App\Http\Controllers\EmailController::class, 'sendEmail'])->name('send.email');
+Route::get('temp',function(){
+    return view('temp');
+});
